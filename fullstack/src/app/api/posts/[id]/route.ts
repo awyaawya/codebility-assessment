de6aed812f2b3b7049b452f6078d6c97f0748d31
aber <abepis180@gmail.com>
2025-10-28 20:45:39 +0800

    bug fix on build

--- fullstack/src/app/api/posts/[id]/route.ts
+++ fullstack/src/app/api/posts/[id]/route.ts
@@ -1,11 +1,11 @@
 import { posts } from '@/data/posts'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 // this will return specific post based on id
-export async function GET(request: Request, { params }: { params: Promise<{ id: number }> }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
 
-    const post = posts.find((p) => p.id == id)
+    const post = posts.find((p) => p.id === Number(id))
 
     if (!post) {
         return NextResponse.json({ error: 'Post Not Found' }, { status: 404 })
